b9c4c0022e4391b2546baa569c687b857385fa3d
Artem K <mihocc18@gmail.com>
2022-08-08 10:31:46 +0300

    Добавляет задачу 4

---
+++ task4-maxOfThree/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] a = new int[3];
+Console.Write("Введите первое число: ");
+a[0] = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+a[1] = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+a[2] = Convert.ToInt32(Console.ReadLine());
+
+int max = a[0];
+
+for (int i = 1; i < a.Length; i++)
+{
+    if (a[i] > max)
+    {
+        max = a[i];
+    }
+}
+Console.WriteLine(max);
+
