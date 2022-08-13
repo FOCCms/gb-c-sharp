b13ecb06670e079ed9368afe5408ce6b75cdf4a9
Artem K <mihocc18@gmail.com>
2022-08-13 16:42:06 +0300

    Добавляет задачу 10

---
+++ task10-getSecondNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿Boolean isNumberValid = false;
+int n;
+do
+{
+    Console.Write("Введите трёхзначное число: ");
+    n = Convert.ToInt32(Console.ReadLine());
+
+    if (Math.Abs(n) / 100 > 0 && Math.Abs(n) / 100 < 10)
+    {
+        isNumberValid = true;
+    }
+} while (!isNumberValid);
+
+Console.WriteLine($"{n} -> {Math.Abs(n) % 100 / 10}");
