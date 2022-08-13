7071a4413bce9eb63beef9f68ea1203117bc2201
Artem K <mihocc18@gmail.com>
2022-08-13 18:50:22 +0300

    Добавляет задание 21

---
+++ task21-distanceBetweenPoints/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите Ax: ");
+int ax = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Ay: ");
+int ay = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Az: ");
+int az = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите Bx: ");
+int bx = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите By: ");
+int by = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Bz: ");
+int bz = Convert.ToInt32(Console.ReadLine());
+
+double distance = Math.Round(Math.Sqrt(Math.Pow(bx - ax, 2) + Math.Pow(by - ay, 2) + Math.Pow(bz - az, 2)), 2);
+Console.WriteLine($"A ({ax},{ay},{az}); B ({bx},{by},{bz}) -> {distance}");
