ab129245edff438d387f9d36b21b4614534e071d
Artem K <mihocc18@gmail.com>
2022-08-13 18:54:47 +0300

    Добавляет задачу 23

---
+++ task23-pow3series/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"{n} -> ");
+for (int i = 1; i <= n; i++)
+{
+    Console.Write($"{Math.Pow(i, 3)}, ");
+}
