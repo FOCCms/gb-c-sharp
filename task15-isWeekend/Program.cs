d6579a23d396ace765171b1dba2b0f12d09eaa87
Artem K <mihocc18@gmail.com>
2022-08-13 16:56:21 +0300

    Добавляет задачу 15

---
+++ task15-isWeekend/Program.cs
@@ -0,0 +1,1 @@
+﻿Boolean isDayValid = false;int day;do{    Console.Write("Введите день недели: ");    day = Convert.ToInt32(Console.ReadLine());    if (day > 0 && day <= 7)    {        isDayValid = true;    }} while (!isDayValid);if (day == 6 || day == 7){    Console.WriteLine($"{day} -> да");}else{    Console.WriteLine($"{day} -> нет");}
