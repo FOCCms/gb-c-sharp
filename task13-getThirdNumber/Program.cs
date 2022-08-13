27748a2efca16f5b3ae9555cb2fdc19c7b01251e
Artem K <mihocc18@gmail.com>
2022-08-13 16:49:38 +0300

    Добавляет задачу 13

---
+++ task13-getThirdNumber/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (Math.Abs(n) / 100 == 0)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    Console.WriteLine($"{n} -> {Math.Abs(n) % 1000 / 100}");
+}
