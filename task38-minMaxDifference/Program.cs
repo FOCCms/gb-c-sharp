dba656f0c48421aeace3591b1634aa58b15882c5
Artem K <mihocc18@gmail.com>
2022-08-20 17:14:52 +0300

    Добавляет задачу 38

---
+++ task38-minMaxDifference/Program.cs
@@ -0,0 +1,40 @@
+﻿int size = 8;
+double[] array = GetRandomArray(size, 0, 100);
+
+Console.WriteLine($"{StringifyArray(array)} -> {Diff(array)}");
+
+double[] GetRandomArray(int size, int min, int max)
+{
+    double[] array = new double[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Math.Round(new Random().NextDouble() * (max - min) + min, 2);
+    }
+
+    return array;
+}
+
+string StringifyArray(double[] array)
+{
+    return $"[{string.Join(", ", array)}]";
+}
+
+double Diff(double[] array)
+{
+    double min = array[0];
+    double max = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+
+    return max - min;
+}
