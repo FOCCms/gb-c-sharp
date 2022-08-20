26e5f1434263fb7c5d71786c5ade0e73e220acd8
Artem K <mihocc18@gmail.com>
2022-08-20 16:30:46 +0300

    Добавляет задачу 27

---
+++ task27-sumOfDigits/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{n} -> {SumOfDigits(n)}");
+
+int SumOfDigits(int n)
+{
+    n = Math.Abs(n);
+
+    int sum = 0;
+
+    while (n > 0)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+
+    return sum;
+}
