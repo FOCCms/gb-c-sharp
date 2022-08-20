2b614fad42a31810f1760a609fad16790310f0f5
Artem K <mihocc18@gmail.com>
2022-08-20 16:41:50 +0300

    Добавляет задачу 29

---
+++ task29-randomArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int size = 8;
+int[] array = new int[size];
+
+PrintArray(GetRandomArray(array));
+
+int[] GetRandomArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+}
