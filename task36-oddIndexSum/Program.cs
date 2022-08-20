0b86fe99e38f6a3c8d8acd331222a933ad6c1362
Artem K <mihocc18@gmail.com>
2022-08-20 17:00:36 +0300

    Добавляет задачу 36

---
+++ task36-oddIndexSum/Program.cs
@@ -0,0 +1,31 @@
+﻿int size = 8;
+int[] array = GetRandomArray(size, 0, 100);
+
+Console.WriteLine($"{StringifyArray(array)} -> {OddIndexSum(array)}");
+
+int[] GetRandomArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+
+    return array;
+}
+
+string StringifyArray(int[] array)
+{
+    return $"[{string.Join(", ", array)}]";
+}
+
+int OddIndexSum(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        sum += array[i];
+    }
+
+    return sum;
+}
