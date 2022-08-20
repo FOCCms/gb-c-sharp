618d71abaae3c688ca61b7a8926fbf09336159bd
Artem K <mihocc18@gmail.com>
2022-08-20 16:56:47 +0300

    Добавляет задачу 34

---
+++ task34-countEven/Program.cs
@@ -0,0 +1,34 @@
+﻿int size = 8;
+int[] array = GetRandomArray(size, 100, 1000);
+
+Console.WriteLine($"{StringifyArray(array)} -> {CountEven(array)}");
+
+int[] GetRandomArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+
+    return array;
+}
+
+int CountEven(int[] array)
+{
+    int count = 0;
+    foreach (int n in array)
+    {
+        if (n % 2 == 0)
+        {
+            count += 1;
+        }
+    }
+
+    return count;
+}
+
+string StringifyArray(int[] array)
+{
+    return $"[{string.Join(", ", array)}]";
+}
