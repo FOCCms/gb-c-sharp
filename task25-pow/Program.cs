0b25bef9eaca5109f883fb7066a6ac0cdd517b24
Artem K <mihocc18@gmail.com>
2022-08-20 16:22:23 +0300

    Добавляет задачу 25

---
+++ task25-pow/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{a}^{b} -> {Pow(a,b)}");
+
+ulong Pow(int a, int b)
+{
+    ulong pow = 1;
+    for (int i = 0; i < b; i++)
+    {
+        pow *= (ulong) a;
+    }
+    return pow;
+}
