e359e52e651ebfb9065413ec7a15327d35e56350
Artem K <mihocc18@gmail.com>
2022-08-28 20:57:11 +0300

    Добавляет задачу 41

---
+++ task41-belowZeroCounter/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите количество чисел: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"Введите число {i + 1}: ");
+    int tmp = Convert.ToInt32(Console.ReadLine());
+
+    if (tmp > 0)
+    {
+        count++;
+    }
+
+    array[i] = tmp;
+}
+
+Console.WriteLine($"{StringifyArray(array)} -> {count}");
+
+
+string StringifyArray(int[] array)
+{
+    return $"{string.Join(", ", array)}";
+}
